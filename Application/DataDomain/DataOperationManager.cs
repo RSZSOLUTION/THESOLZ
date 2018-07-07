5992f5bfdc7592da151255f6f5aa9f4c4e04f69f
RSZSOLUTION <rszsolution@gmail.com>
2018-07-08 00:49:36 +0530

    RSZ Git 3

---
+++ Application/DataDomain/DataOperationManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSZData
+{
+	public class DataOperationManager : IDataOperationManager
+	{
+		private bool _disposed;
+
+		public DataOperationManager()
+		{
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (_disposed)
+				return;
+			if (disposing)
+			{
+			}
+			_disposed = true;
+		}
+
+		~DataOperationManager()
+		{
+			Dispose(false);
+		}
+	}
+}
